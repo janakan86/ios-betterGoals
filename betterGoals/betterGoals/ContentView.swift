5f34a163da76da7ffd8465ea83f63b3db20a1df7
Jana <janakan86@gmail.com>
2020-05-01 16:06:19 +1000

    empty project

---
+++ betterGoals/betterGoals/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  betterGoals
+//
+//  Created by K Janakan on 1/5/20.
+//  Copyright © 2020 K Janakan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
