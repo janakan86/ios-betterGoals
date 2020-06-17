8a95dc3927551fa8e4f7b363d85ab77bff593e46
Jana <janakan86@gmail.com>
2020-06-17 17:15:29 +1000

    Introduce a new image for the background

---
+++ betterGoals/betterGoals/Views/Goals/createGoals.swift
@@ -0,0 +1,27 @@
+//
+//  createGoals.swift
+//  betterGoals
+//
+//  Created by K Janakan on 17/6/20.
+//  Copyright © 2020 K Janakan. All rights reserved.
+//
+
+import SwiftUI
+
+struct createGoalsStep1: View {
+    var body: some View {
+        VStack{
+            
+        Image("mountain")
+             .resizable()
+             .scaledToFit()
+        
+        }
+    }
+}
+
+struct createGoals_Previews: PreviewProvider {
+    static var previews: some View {
+        createGoalsStep1()
+    }
+}
