50990bc5436acbabf7d950a4813a6dac365e3f4a
Jana <janakan86@gmail.com>
2020-06-18 17:59:13 +1000

    Introduce a new View to display the questions

--- betterGoals/betterGoals/Views/Goals/createGoals.swift
+++ betterGoals/betterGoals/Views/Goals/createGoals.swift
@@ -10,18 +10,47 @@ import SwiftUI
 
 struct createGoalsStep1: View {
     var body: some View {
-        VStack{
+        VStack(){
             
         Image("mountain")
              .resizable()
              .scaledToFit()
+              
         
         }
+        .padding(5)
+    }
+}
+
+
+struct createGoalsQuestion: View {
+    @State var questionResponse: String
+    var questionPlaceHolder:String
+    var questionLabel:String
+    
+    var body: some View {
+        VStack{
+            
+            Text(questionLabel).font(Font.system(size: 15, weight: .medium, design: .serif))
+                .foregroundColor(Color("purple"))
+            
+            
+            TextField(questionPlaceHolder, text: $questionResponse)
+                .textFieldStyle((RoundedBorderTextFieldStyle()))
+                .padding()
+        }
+        
+    
     }
 }
 
 struct createGoals_Previews: PreviewProvider {
     static var previews: some View {
-        createGoalsStep1()
+        createGoalsQuestion(questionResponse:"",
+             questionPlaceHolder:"My Goal is ...",
+             questionLabel:"What is your Goal?")
     }
 }
+
+
+
