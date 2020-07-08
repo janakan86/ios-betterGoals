f19ce875eef8479ac99c8e5382b951504099ed2c
Jana <janakan86@gmail.com>
2020-07-08 18:08:16 +1000

    checkmark when the list is tapped

--- betterGoals/betterGoals/Views/ItemTypeList.swift
+++ betterGoals/betterGoals/Views/ItemTypeList.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct ItemTypeList: View {
     
+    
     @State var selectedItem:ItemUI?
     
     var itemUIValues:[ItemUI] = ItemTypesUIDefaults.getAllItemUIValues()
@@ -19,54 +20,50 @@ struct ItemTypeList: View {
         List(itemUIValues)
         { itemUIValue in
             
-            ItemTypeRow(itemUI: itemUIValue)
-                .onTapGesture {
-                    self.selectedItem = itemUIValue
+            HStack{
+                
+                Image(itemUIValue.defaultIcon)
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                //.cornerRadius(8)
+                
+                Text(itemUIValue.name)
+                    .foregroundColor(Color("pink"))
+                    .font(.title)
+                    .padding(.top,10)
+                    .padding(.leading,10)
+                
+                Spacer()
+                
+                if(itemUIValue.id == self.selectedItem?.id){
+                    
+                    Image(systemName: "checkmark")
+                        .imageScale(.medium)
+                        .foregroundColor(Color("pink"))
                 }
-                .background(
-                    self.selectedItem?.id == itemUIValue.id ? Color("lightPink") : Color(.white))
-                .cornerRadius(6)
-                .shadow(color:Color("lightPink"), radius: 1, x: 1, y: 1)
+                
+            }
+                
+            //tap
+            .contentShape(Rectangle())//define tappable area
+            .onTapGesture {
+                self.selectedItem = itemUIValue
+            }
+                
+            //style
+            .cornerRadius(6)
+            .shadow(color:Color("lightPink"), radius: 1, x: 1, y: 1)
+            
             
         }
+            
         .onAppear { UITableView.appearance().separatorStyle = .none }
         
-                    
-                    
-            
-                
-            
-        }
     }
-
-
-struct ItemTypeRow : View {
     
-    var itemUI:ItemUI;
-    
-    var body: some View {
-        
-        HStack{
-            
-            Image(itemUI.defaultIcon)
-                 .resizable()
-                 .frame(width: 50, height: 50)
-                 //.cornerRadius(8)
-                
-            Text(itemUI.name)
-                .foregroundColor(Color("pink"))
-                .font(.title)
-                .padding(.top,10)
-                .padding(.leading,10)
-            
-            Spacer()
-            
-        }
-        .contentShape(Rectangle()) //makes the spacer clickable/ selectable
-       
-    }
 }
 
+ 
 struct ItemTypeList_Previews: PreviewProvider {
     static var previews: some View {
         ItemTypeList()
